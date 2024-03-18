adf9bd32c05420fa9b0cf18e414d1d6916c2cd35
Ivan Kysiel <keselivdaf@gmail.com>
2024-03-18 17:47:34 +0100

    denys

--- project-denis/script.js
+++ project-denis/script.js
@@ -11,3 +11,9 @@ window.addEventListener('scroll', () => {
       }
     });
 });
+
+$(document).ready(function() {
+  $('.header__burger').click(function(event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+  });
+});
